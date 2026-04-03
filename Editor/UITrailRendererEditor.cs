66f4cc11cfe2550d02739298e121cb9a0e811dfc
Dmitry <lazarev.dmitriy91@gmail.com>
2026-04-04 01:36:33 +0300

    Create UITrailRendererEditor.cs

---
+++ Editor/UITrailRendererEditor.cs
@@ -0,0 +1,11 @@
+	[CustomEditor(typeof(UITrailRenderer))]
+	[CanEditMultipleObjects]
+	public class UITrailRendererEditor : Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			serializedObject.Update();
+			DrawPropertiesExcluding(serializedObject, "m_Script", "m_OnCullStateChanged", "m_RaycastPadding");
+			serializedObject.ApplyModifiedProperties();
+		}
+	}
