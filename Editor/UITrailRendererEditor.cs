1d18bbc9284805e41e5972f89cea6cd19af75499
Dmitry <lazarev.dmitriy91@gmail.com>
2026-04-04 01:47:17 +0300

    Replace fade curve with gradients and add UV modes

--- Editor/UITrailRendererEditor.cs
+++ Editor/UITrailRendererEditor.cs
@@ -1,3 +1,7 @@
+using UnityEditor;
+
+namespace mitaywalle
+{
 	[CustomEditor(typeof(UITrailRenderer))]
 	[CanEditMultipleObjects]
 	public class UITrailRendererEditor : Editor
@@ -9,3 +13,4 @@
 			serializedObject.ApplyModifiedProperties();
 		}
 	}
+}
